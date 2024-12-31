dbd656f1a2ca6b0232750c87e1f101773f57060b
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-12-31 11:16:30 +0800

    使用mongoose模式更改users模型

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
   try{
-    await mongoose.connect(process.env.DATABESE_URI, {
+    await mongoose.connect(process.env.DATABASE_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     })
